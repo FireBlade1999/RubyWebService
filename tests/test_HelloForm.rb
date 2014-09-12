03c758104eea13ed301636f75fa7c6dcbca370c2
Steve Williams <steven.williams@bjss.com>
2014-09-12 11:53:36 +0100

    Index, input form, tests and doc added

---
+++ tests/test_HelloForm.rb
@@ -0,0 +1,40 @@
+
+
+############################################################
+#
+# To test a web app you can use 'Rack::Test' gem.
+#
+# This test pretends to be a web browser, visiting 
+# the web application using code.
+#
+############################################################
+
+require './bin/RubyWebService.rb'
+require 'test/unit'
+require 'rack/test'
+
+class HelloFormTest < Test::Unit::TestCase
+
+	include Rack::Test::Methods
+
+	def app
+		Sinatra::Application
+	end
+
+	def test_index
+		get '/'
+		assert_equal 'Hello World!', last_response.body
+	end
+
+	def test_hello_form
+		get '/hello/'
+		assert last_response.ok?
+		assert last_response.body.include?('A greeting')
+	end
+
+	def test_hello_form_post
+		post '/hello/', params={:name=>'Frank', :greeting=>"Hi"}
+		assert last_response.ok?
+		assert last_response.body.include?('I just wanted to try a web app with Ruby')
+	end
+end
